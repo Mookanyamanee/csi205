38f3dd5e4f506fbd3d7265de853165e052d02318
Mookanyamanee <mookanyamanee.kae@spumail.net>
2025-11-11 23:28:01 +0700

    delete source code

--- src/component/AppFooter.jsx
+++
@@ -1,8 +0,0 @@
-const AppFooter = () => {
-    return ( <>
-    <h4 className="d-flex justify-content-center">มหาวิทยาลัยศรีปทุม คณะเทคโนโลยีสารสนเทศ สาขาวิทยาการคอมพิวเตอร์และนวัตกรรมพัฒนาซอฟแวร์</h4>
-    <h4 className="d-flex justify-content-center">IG : lkicbslibcjihhocsihwnocctilbat</h4>
-    </> );
-}
- 
-export default AppFooter;
--- src/component/AppHeader.jsx
+++
@@ -1,7 +0,0 @@
-const AppHeader = () => {
-    return ( <>
-    <h1 className="d-flex justify-content-center">67112209 CSI205</h1>
-    </> );
-}
- 
-export default AppHeader;
