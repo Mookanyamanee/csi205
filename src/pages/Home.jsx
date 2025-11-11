7e66cea7a5cd67e46132123b93771c3b4917aa02
Mookanyamanee <mookanyamanee.kae@spumail.net>
2025-11-11 22:49:22 +0700

    gdf

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
-import MookImg from "../img/Mook.jpeg"
+
 
 const Home = () => {
     return ( <>
         <div className="text-center">
-            <img src={MookImg} alt="" className="mt-3" style={{width : "200px"}}/>
+            <img src="img/Mook.jpeg" alt="" className="mt-3" style={{width : "200px"}}/>
             <h5 className="mb-3 text-primary">67112209 นางสาวมุกอัญญมณี แก้วลาดวงษ์ <br></br>ปี2 สาขาวิทยาการคอมพิวเตอร์และนวัตกรรมพัฒนาซอฟแวร์ คณะเทคโนโลยีสารสนเทศ มหาวิทยาลัยศรีปทุม<br />สวัสดีค่ะ</h5>
         </div>
     </>);
